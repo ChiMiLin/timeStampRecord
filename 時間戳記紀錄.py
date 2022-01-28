53049cc313b41ad66b5b618bdfdfe9d9256b4958
林一源 <charles01270@gmail.com>
2022-01-28 14:20:18 +0800

    Update 時間戳記紀錄.py
    add preview record
    add start time modify

--- 時間戳記紀錄.py
+++ 時間戳記紀錄.py
@@ -2,12 +2,11 @@ import time
 import os
 filename = input('請輸入紀錄檔檔名 : ')
 
-recordFile = open(filename + '.txt', 'a', encoding = 'utf8')
 
-# recordFile.writeline
 
 tagList = {}
 cnt = 1
+recordLog = []
 
 while(1):
     text = input('請輸入初始化時間戳記類別，結束時請輸入-1 : ')
@@ -17,11 +16,14 @@ while(1):
     cnt += 1
     
 input('輸入任意值以開始記錄時間戳記...')
-recordFile.write(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ' --開始--\n')
+recordLog.append([time.localtime(), 0, '--開始--'])
 ST_TIME = int(time.time())
 
 while(1):
     os.system('cls')
+    for line in recordLog:
+        print(time.strftime("%Y-%m-%d %H:%M:%S", line[0]) + '\t' + str(line[1]) + '\t' + line[2])
+    print('==========')
     print('請輸入編號以標記時間戳')
     for i in tagList.keys():
         print(str(i) + ' : ' + tagList[i])
@@ -34,14 +36,38 @@ while(1):
     diffTimeStr = str(int(diffTime/60)) + '\'' + str(diffTime%60) + '\"'
     
     try:
-        recordFile.write(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + '\t' + diffTimeStr + '\t' + tagList[int(tagNm)] + '\n')
+        recordLog.append([time.localtime(), diffTimeStr, tagList[int(tagNm)]])
     except:
-        recordFile.write(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + '\t' + diffTimeStr + '\t' + tagNm + '\n')
-recordFile.close()
-os.system('cls')
+        recordLog.append([time.localtime(), diffTimeStr, tagNm])
+    os.system('cls')
+
+
+    
+c = input('是否校正紀錄起始時間點? Y/N : ')
+while c.upper() == 'Y':
+    stTime = input('請輸入影片開始時間 : ')
+    for line in recordLog:
+        deltaTime = int(time.mktime(line[0])) - int(time.mktime(time.strptime(stTime, "%Y-%m-%d %H:%M:%S")))
+        if deltaTime < 0:
+            line[1] = '##'
+        else:
+            line[1] = str(int(deltaTime/60)) + '\'' + str(deltaTime%60) + '\"'
+    for line in recordLog:
+        print(time.strftime("%Y-%m-%d %H:%M:%S", line[0]) + '\t' + str(line[1]) + '\t' + line[2])
+    print('==========')
+    ec = input('修正結果是否正確? Y/N : ')
+    if ec.upper() == 'Y':
+        break
+
+print('紀錄開始寫入')
+with open(filename + '.txt', 'a', encoding = 'utf8') as recordFile:
+    for line in recordLog:
+        recordFile.write(time.strftime("%Y-%m-%d %H:%M:%S", line[0]) + '\t' + str(line[1]) + '\t' + line[2] + '\n')
+    recordFile.close()
+    print('紀錄寫入完成')
 
 with open(filename + '.txt', 'r', encoding = 'utf8') as recordFile:
     for line in recordFile:
-        print(line.strip())
+        print(line, end= "")
     recordFile.close()
 input('輸入任意值以離開程式...')
